32ac3f6fc546693c0ca55290d07c6abd122dc67e
Prem kumar Chilumula <129075681+prem878@users.noreply.github.com>
2024-05-23 19:47:53 +0530

    Create BitMasks.cpp

---
+++ BitMasks.cpp
@@ -0,0 +1,37 @@
+#include "bits/stdc++.h"
+using namespace std;
+
+bool isEven(int n){
+  return (n & 1);
+}
+
+int getBit(int& n,int idx){
+  int mask=(1<<idx);
+  int bit = (mask & n) > 0 ? 1 : 0;
+  return bit;
+}
+
+int setBit(int n,int idx){
+  int mask=1<<idx;
+  int bit=(n | mask);
+  return bit;
+}
+
+void clearBit(int& n,int i){
+  int preMask=(1<<i);
+  int mask=~(preMask);
+  n=(n & mask);
+}
+
+void update(int& n,int i,int val){
+  int premask=1<<i;
+  int mask=~premask;
+  n=(n&mask) | val>>i;
+}
+
+int main(){
+  //Perform bit masks
+  
+  return 0;
+}
+
