3bbe4e504697b7d2f93f4b851ebab33aa0958014
Prem kumar Chilumula <129075681+prem878@users.noreply.github.com>
2024-06-12 19:10:26 +0530

    Create BipartiteCheck.cpp

---
+++ BipartiteCheck.cpp
@@ -0,0 +1,32 @@
+#include<bits/stdc++.h>
+using namespace std;
+vector<vector<int>> adj;
+bool isBipartite(){
+  int n=adj.size();
+  bool bipartite=false;
+  vector<int> side(n,-1);
+  Queue<int> q;
+  for(size_t st=0;st<n;++st){
+    if(side[st]==-1){
+      q.push(st);
+      side[st]=1;
+      while(q.isEmpty()==false){
+        int v=q.front();
+        q.pop();
+        for(int u : adj[v]){
+          if(side[u]==-1){
+            side[u]=side[v]^1;
+            q.push(u);
+          }else{
+            bipartite & = side[u]!=sode[v];
+          }
+        }
+      }
+    }
+  }
+  return biparitie;
+}
+int main(){
+  //adj
+  return 0;
+}
