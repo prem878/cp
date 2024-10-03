5a4abbde9cc396f040367132dc8ed176c64765fe
Prem kumar Chilumula <129075681+prem878@users.noreply.github.com>
2024-10-03 21:22:14 +0530

    comment line 3 and 4 for console output

---
+++ Hacker-Cup-template.cpp
@@ -0,0 +1,17 @@
+#include<bits/stdc++.h>
+#define ll long long
+#define cout fout
+#define cin fin
+using namespace std;
+int main(){
+	ifstream fin("in.txt");
+	ofstream fout("out.txt");
+	int t;
+	cin>>t;
+	for(size_t i=1;i<=t;++i){
+		cout<<"Case #"<<i<<": ";
+
+		cout<<"\n";
+	}
+	return 0;
+}
