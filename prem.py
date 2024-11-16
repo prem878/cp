36dcd222c5a3b21d1e1888b5074d938339736b3c
Prem kumar Chilumula <129075681+prem878@users.noreply.github.com>
2024-11-17 00:06:50 +0530

    Create prem.py

---
+++ prem.py
@@ -0,0 +1,21 @@
+import cv2
+from cv2 import dnn_superres
+ 
+# Create an SR object - only function that differs from c++ code
+sr = dnn_superres.DnnSuperResImpl_create()
+ 
+# Read image
+image = cv2.imread('./image.png')
+ 
+# Read the desired model
+path = "EDSR_x4.pb"
+sr.readModel(path)
+ 
+# Set the desired model and scale to get correct pre- and post-processing
+sr.setModel("edsr", 4)
+ 
+# Upscale the image
+result = sr.upsample(image)
+ 
+# Save the image
+cv2.imwrite("./upscaled.png", result)
